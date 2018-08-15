a8910f48910929d6c23028b5bdfc1d0a87d556ac
Lior Gonda <lior@gondas.com>
2018-08-14 20:56:36 -0400

    page(work)

--- js/app.js
+++ js/app.js
@@ -1,10 +1,32 @@
+// GLOBAL
 (function(){
-  $('#home #menu a').hover(
-    (event) => {
-      console.log( $(event.target).attr('video-class') )
-    },
-    (event) => {
-      console.log( 'default' )
-    }
-  );
+  var video = 'http://www.maverickdigital.com' + $('#background-container').attr('vidsrc');
+  $('#background-container video').attr('src', video);
 })();
+
+// HOMEPAGE
+if ( $('#home').length ) {
+  (function(){
+
+
+    $('#home #menu a').hover(
+      (event) => {
+        var video = 'http://www.maverickdigital.com' + $(event.target).parent().attr('vidsrc');
+        $('#background-container video').attr('src', video);
+      },
+      (event) => {
+        var video = 'http://www.maverickdigital.com' + $('#background-container').attr('vidsrc');
+        $('#background-container video').attr('src', video);
+      }
+    );
+  })();
+}
+
+function toggleMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
