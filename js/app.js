639a2f528ca3aa3cab2e42fa39b67481e67f4db1
Rob Gonda <rob@robgonda.com>
2018-08-27 19:23:14 -0500

    HLS

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 // GLOBAL
 (function(){
-  var video = 'http://www.maverickdigital.com' + $('#background-container').attr('vidsrc');
+  var video = $('#background-container').attr('vidsrc');
   $('#background-container video').attr('src', video);
+
+  //playVideo(video);
+
   // active topnav
   var activeLink = $('#myTopnav a[href="' + document.location.href + '"]'),
       allLinks = $('#myTopnav a');
@@ -34,12 +37,14 @@ if ( $('#home').length ) {
   (function(){
     $('#home #menu a').hover(
       (event) => {
-        var video = 'http://www.maverickdigital.com' + $(event.target).parent().attr('vidsrc');
+        var video = $(event.target).parent().attr('vidsrc');
         $('#background-container video').attr('src', video);
+        //playVideo(video);
       },
       (event) => {
-        var video = 'http://www.maverickdigital.com' + $('#background-container').attr('vidsrc');
+        var video = $('#background-container').attr('vidsrc');
         $('#background-container video').attr('src', video);
+        //playVideo(video);
       }
     );
   })();
@@ -89,6 +94,33 @@ if ( $('#home').length ) {
   }
 }
 
+
+function playVideo(source) {
+
+  // Video
+    var video = document.getElementById('video');
+    if(Hls.isSupported()) {
+      var hls = new Hls();
+      hls.loadSource(source);
+      hls.attachMedia(video);
+      hls.on(Hls.Events.MANIFEST_PARSED,function() {
+        video.play();
+    });
+   }
+   // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
+   // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
+   // This is using the built-in support of the plain video element, without using hls.js.
+   // Note: it would be more normal to wait on the 'canplay' event below however on Safari (where you are most likely to find built-in HLS support) the video.src URL must be on the user-driven
+   // white-list before a 'canplay' event will be emitted; the last video event that can be reliably listened-for when the URL is not on the white-list is 'loadedmetadata'.
+    else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+      video.src = '/assets/video/home/home.m3u8';
+      video.addEventListener('loadedmetadata',function() {
+        video.play();
+      });
+    }
+}
+
+
 // about
 if ( $('#about').length ) {
   (function(){
@@ -105,3 +137,6 @@ if ( $('#services').length ) {
     new Foundation.Accordion($('.accordion'), {});
   })();
 }
+
+
+
